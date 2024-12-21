b4b19b6c77ca02d3b7b584c72b6676baa3c61597
Timur <timur.buchkin@mail.ru>
2024-12-21 11:39:23 +0300

    add initial db service files

---
+++ ml/app.py
@@ -0,0 +1,31 @@
+#!venv/bin/python
+from typing import Optional, List
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import src.db.sessionManager as sm
+from src.core.config import DB_CONFIG
+import json
+
+
+sessionManager = sm.SessionManager(DB_CONFIG)
+
+app = FastAPI()
+
+# origins = [
+#         "*"
+#         ]
+#
+# app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=origins,
+#         allow_credentials=True,
+#         allow_headers=['*'],
+#         allow_methods=['*'],
+#         )
+
+@app.get("/")
+def forecast(items: Optional[List[int]] = None):
+    """Do forecast for `items` if given else for all existing items"""
+    pass
+
